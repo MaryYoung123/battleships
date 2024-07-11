d6746be59276b4bddedaac4e37a15beec3a84bcd
Mary Young <itsmaryyyyy@gmail.com>
2024-07-11 21:34:45 +0000

    add function to get username

--- run.py
+++ run.py
@@ -3,3 +3,15 @@ from random import randrange
 ship_initial = ["B", "C", "F", "A", "S"]
 ship_names = ["Battleship", "Cruiser", "Frigate", "Aircraft Carrier", "Sub"]
 map_size = 10
+
+def get_username():
+    """
+    To ask user to input their name
+    """
+    while True:
+        user_name = input("Please enter your name, comrade!: ")
+        if user_name:
+            print(f"Welcome to battleships, {user_name}!")
+            return user_name
+        else:
+            print("Please enter your name!")
